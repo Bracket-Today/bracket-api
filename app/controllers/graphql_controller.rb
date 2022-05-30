b86d7b6e0f36f1d6c397869cde6fbce66edafd6b
Jared Morgan <jmorgan@morgancreative.net>
2022-05-30 14:33:16 -0500

    Set current_user from x-uuid header

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -9,8 +9,7 @@ class GraphqlController < ApplicationController
     query = params[:query]
     operation_name = params[:operationName]
     context = {
-      # Query context goes here, for example:
-      # current_user: current_user,
+      current_user: User.by_uuid(request.headers['HTTP_X_UUID'])
     }
     result = BracketSchema.execute(query, variables: variables, context: context, operation_name: operation_name)
     render json: result
