d53f6e416c11c7a75a016bf566041b106e16cb69
Jared Morgan <jmorgan@morgancreative.net>
2022-06-06 20:52:00 -0500

    ContestType - add isActive and currentUserVote

--- app/graphql/types/contest_type.rb
+++ app/graphql/types/contest_type.rb
@@ -9,5 +9,15 @@ module Types
     field :upper, Types::CompetitorType, null: true
     field :lower, Types::CompetitorType, null: true
     field :winner, Types::CompetitorType, null: true
+    field :is_active, GraphQL::Types::Boolean, null: false
+    field :current_user_vote, Types::CompetitorType, null: true
+
+    def is_active
+      object.active?
+    end
+
+    def current_user_vote
+      object.votes.find_by(user_id: context[:current_user].id).try(:competitor)
+    end
   end
 end
