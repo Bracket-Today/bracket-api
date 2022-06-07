f6f29340b096e6b14cef006ec2b307aa3af52a0a
Jared Morgan <jmorgan@morgancreative.net>
2022-06-06 20:52:12 -0500

    Add submitVote mutation

---
+++ app/graphql/mutations/contests/submit_vote.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+module Mutations
+  module Contests
+    class SubmitVote < Mutations::BaseMutation
+      argument :contest_id, ID, required: true, loads: Types::ContestType,
+        as: :contest
+      argument :competitor_id, ID, required: true, loads: Types::CompetitorType,
+        as: :competitor
+
+      field :contest, Types::ContestType, null: false
+
+      def resolve contest:, competitor:
+        vote = contest.votes.where(user_id: context[:current_user].id).
+          first_or_initialize
+        vote.competitor = competitor
+        vote.save!
+
+        { contest: contest }
+      end
+    end
+  end
+end
