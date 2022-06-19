4ae669ff872ae2d5fa3dd0798e1b21544b3f5983
Jared Morgan <jmorgan@morgancreative.net>
2022-06-18 21:11:13 -0500

    Production credentials

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3008'
+    origins(*(Rails.application.credentials.allowed_origins))
 
     resource '*',
       headers: :any,
