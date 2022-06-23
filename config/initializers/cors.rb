de4f2c272e8c7385c39bef82d374dd4a7504a204
Jared Morgan <jmorgan@morgancreative.net>
2022-06-22 19:47:42 -0500

    Expose X-Request-Id for front end issue tracking

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -11,6 +11,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
 
     resource '*',
       headers: :any,
+      expose: ['X-Request-Id'],
       methods: [:post, :options, :head]
   end
 end
