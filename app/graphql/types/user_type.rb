1cd467d0b1d7fcc718779376bf9575e6ba446e50
Jared Morgan <jmorgan@morgancreative.net>
2022-07-12 20:37:14 -0500

    1f8a3a - UserType - tournament data for editing

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -4,8 +4,15 @@ module Types
   class UserType < Types::BaseObject
     field :id, ID, null: false
     field :login_code, String, null: false
+    field :tournament, Types::TournamentType, null: true do
+      argument :id, ID, required: true
+    end
     field :tournaments, [Types::TournamentType], null: false
     field :votes_count, Int, null: false
+
+    def tournament id:
+      object.tournaments.find_by_id(id)
+    end
 
     def votes_count
       object.votes.count
