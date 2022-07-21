2a7aaa74d27104b5788d13940f743eece55cabb4
Mike Morgan <mike.morgan@preflighttech.com>
2022-07-20 20:24:46 -0500

    61644b - Posting the winner to twitter as well

--- app/services/tournament_service/close_round.rb
+++ app/services/tournament_service/close_round.rb
@@ -15,6 +15,14 @@ module TournamentService
 
       if @tournament.contests.where(winner_id: nil).empty?
         @tournament.update(status: 'Closed')
+        # Send a message to Twitter
+        client = Twitter::REST::Client.new do |config|
+          config.consumer_key        = Rails.application.credentials.dig(:twitter, :key)
+          config.consumer_secret     = Rails.application.credentials.dig(:twitter, :key_secret)
+          config.access_token        = Rails.application.credentials.dig(:twitter, :user_access_token)
+          config.access_token_secret = Rails.application.credentials.dig(:twitter, :user_token_secret)
+        end
+        client.update("The winner of the #{@tournament.name} bracket is #{@tournament.contests.last.winner.entity.name}. https://bracket.today/bracket/#{@tournament.id}")
       end
     end
   end
