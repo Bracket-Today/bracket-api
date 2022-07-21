cc4ebbdf08bac8a09158d1c9e65acc8f3e135aa0
Mike Morgan <mike.morgan@preflighttech.com>
2022-07-20 20:06:50 -0500

    55364c - Setting up Twitter to automatically send a tweet to @bracket.today when a new bracket is out.

--- app/services/tournament_service/update_rounds.rb
+++ app/services/tournament_service/update_rounds.rb
@@ -8,6 +8,15 @@ module TournamentService
       Tournament.ready_to_activate.each do |tournament|
         tournament.create_contests! if tournament.contests.empty?
         tournament.update!(status: 'Active')
+
+        # Send a message to Twitter
+        client = Twitter::REST::Client.new do |config|
+          config.consumer_key        = Rails.application.credentials.dig(:twitter, :key)
+          config.consumer_secret     = Rails.application.credentials.dig(:twitter, :key_secret)
+          config.access_token        = Rails.application.credentials.dig(:twitter, :user_access_token)
+          config.access_token_secret = Rails.application.credentials.dig(:twitter, :user_token_secret)
+        end
+        client.update("Today's bracket is out! Vote for #{tournament.name} now. https://bracket.today/bracket/#{tournament.id}")
       end
 
       Tournament.active.all.each do |tournament|
