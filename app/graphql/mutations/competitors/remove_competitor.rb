dd156d6e192cbb1f91a7d90915bdca9ce7f4b14a
Jared Morgan <jmorgan@morgancreative.net>
2022-07-31 17:50:05 -0500

    5fef30 - RemoveCompetitor mutation

---
+++ app/graphql/mutations/competitors/remove_competitor.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+module Mutations
+  module Competitors
+    class RemoveCompetitor < Mutations::BaseMutation
+      description <<-DESCRIPTION
+        Remove competitor from a tournament. If this is the tournament for
+        the entity, also delete the entity.
+      DESCRIPTION
+
+      argument :id, ID, required: true, loads: Types::CompetitorType,
+        as: :competitor
+
+      field :competitor, Types::CompetitorType, null: true
+      field :errors, [Types::UserError], null: false
+
+      def resolve competitor:
+        entity = competitor.entity
+        result = destroy_resource :competitor, competitor
+
+        entity.destroy if entity.competitors.empty?
+
+        result
+      end
+    end
+  end
+end
--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -2,6 +2,8 @@ module Types
   class MutationType < Types::BaseObject
     field :create_competitor,
       mutation: Mutations::Competitors::CreateCompetitor
+    field :remove_competitor,
+      mutation: Mutations::Competitors::RemoveCompetitor
 
     field :create_tournament,
       mutation: Mutations::Tournaments::CreateTournament
