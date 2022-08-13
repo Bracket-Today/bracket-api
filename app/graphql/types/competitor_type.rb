de5705ba2da24b1e7722fbe5fcf09d45e1ac514e
Mike Morgan <mike.morgan@preflighttech.com>
2022-08-13 17:57:50 -0500

    bee5a5 - Adding the backend to show a basic list of the votes per competitor.

--- app/graphql/types/competitor_type.rb
+++ app/graphql/types/competitor_type.rb
@@ -6,5 +6,15 @@ module Types
     field :tournament, Types::TournamentType, null: false
     field :entity, Types::EntityType, null: false
     field :seed, Int, null: true
+
+    field :votes, String, null: true
+
+    def votes
+      vote_array = []
+      object.tournament.contests.each do |c|
+        vote_array << "Round #{c.round}: #{object.votes.where(contest_id: c.id).count}"
+      end
+      vote_array
+    end
   end
 end
