f517dd41a5de08dde3a78930ab9d47c17e151d3c
Mike Morgan <mike.morgan@preflighttech.com>
2022-08-13 18:08:45 -0500

    bee5a5 - Adding some logging so I can find the unexpected JSON input

--- app/graphql/types/competitor_type.rb
+++ app/graphql/types/competitor_type.rb
@@ -10,8 +10,12 @@ module Types
     field :votes, String, null: true
 
     def votes
+      Rails.logger.info("!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!")
+      Rails.logger.info("inside votes array")
       vote_array = []
       object.tournament.contests.each do |c|
+        Rails.logger.info(c.inspect)
+        Rails.logger.info("Round #{c.round}: #{object.votes.where(contest_id: c.id).count}")
         vote_array << "Round #{c.round}: #{object.votes.where(contest_id: c.id).count}"
       end
       vote_array.join(" | ")
