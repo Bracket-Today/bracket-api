a0026208e9db7b84878f459886dceedefea17b85
Jared Morgan <jmorgan@morgancreative.net>
2023-04-21 19:50:48 -0500

    89b210 - Reminder emails - Add ASM group

--- app/mailers/reminder_mailer.rb
+++ app/mailers/reminder_mailer.rb
@@ -4,7 +4,8 @@ class ReminderMailer < ApplicationMailer
   def vote user:
     mail(
       to: user.email,
-      subject: 'Time to vote on bracket.today'
+      subject: 'Time to vote on bracket.today',
+      asm: { group_id: 26087 }
     )
   end
 end
